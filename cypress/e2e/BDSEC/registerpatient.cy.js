249567af8da932d61a28211f644f4ce53f088e06
Muyundo <muyundobrian@gmail.com>
2025-01-21 10:21:44 +0300

    updated lab module automation

--- cypress/e2e/BDSEC/registerpatient.cy.js
+++ cypress/e2e/BDSEC/registerpatient.cy.js
@@ -19,6 +19,7 @@ context('Actions', () => {
     const randomAge = Math.floor(Math.random() * 50) + 1
 
     // Start filling the form
+    cy.waitForLoader()
     cy.get('#location').select('Registration Desk')
     cy.get('.confirm').click()
     cy.waitForLoader()
@@ -67,22 +68,24 @@ context('Actions', () => {
       })
 
     // Search for Registered Patient
-    cy.contains('Search').click()
+    cy.get('.back-btn > .fa').click()
+    
+  //  cy.contains('Search').click()
     cy.get('@patientData').then((patientData) => {
-      cy.get('#name').type(`${patientData.fname} ${patientData.lname}`)
-      cy.get(':nth-child(4) > .reg-srch-btn > .ng-binding').click()
+     // cy.get('#name').type(`${patientData.fname} ${patientData.lname}`)
+     // cy.get(':nth-child(4) > .reg-srch-btn > .ng-binding').click()
 
       // Validate search results
-      cy.get('tr.ng-scope > :nth-child(2)').should('contain', patientData.fname)
+      //cy.get('tr.ng-scope > :nth-child(2)').should('contain', patientData.fname)
 
-      cy.get('.back-btn > .fa').click()
+     // cy.get('.back-btn > .fa').click()
       cy.wait(1000)
       cy.get('.fa-stethoscope').click()
       cy.waitForLoader()
       cy.get('#patientIdentifier', { timeout: 10000 }).should('be.visible')
       cy.waitForLoader() 
       cy.get('#patientIdentifier').type(`${patientData.fname}`)
-    })
+    
     cy.waitForLoader()
 
     cy.get('.smallImages').click()
@@ -91,6 +94,7 @@ context('Actions', () => {
     cy.waitForLoader()
 
     cy.get('.btn--left').click()
+    cy.wait(5000)
     cy.contains('Orders').click()
     cy.waitForLoader()
     cy.contains('Arterial lactate').click({ force: true })
@@ -104,5 +108,20 @@ context('Actions', () => {
     cy.get('.opd-header-bottom > :nth-child(1) > bm-back-links > ul > li.ng-scope > #patients-link > .fa').click()
     cy.waitForLoader()
     cy.get('.back-btn > .fa').click()
-   })
+    cy.visit('https://bdsec.intellisoftkenya.com/')
+    cy.get(':nth-child(2) > .launch > a').click()
+    cy.waitForLoader()
+    cy.get(':nth-child(1) > [colspan="2"] > input').type('admin')
+    cy.get(':nth-child(3) > tbody > :nth-child(2) > [colspan="2"] > input').type('adminADMIN!')
+    cy.get('#submitButton').click()
+    cy.get('#todaySamplesToCollectListContainer-slick-grid > .slick-headerrow > .slick-headerrow-columns > .l1 > input').type(`${patientData.fname}`)
+    cy.get('#todaySamplesToCollectListContainer-slick-grid > .slick-viewport > .grid-canvas > [style="top:0px"] > .l8 > a').click()
+    cy.get('[width="80%"] > .textButton').click()
+    cy.wait(500)
+    cy.get('#saveButtonId').click()
+    cy.get('#todaySamplesCollectedListContainerId').click()
+    cy.get('#todaySamplesCollectedListContainer-slick-grid > .slick-headerrow > .slick-headerrow-columns > .l2 > input').type(`${patientData.fname}`)
+    cy.get('[style="top:0px"] > .l12 > #result > #actionIcon').click()
+     })
+    })
 })
