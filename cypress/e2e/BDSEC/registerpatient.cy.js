feb2f1fca7bd0a0ed88a871b6f0c4d2a4fe5e214
Muyundo <muyundobrian@gmail.com>
2025-01-28 17:40:36 +0300

    Updated Before Surgery Checklist and Color Vision Test

--- cypress/e2e/BDSEC/registerpatient.cy.js
+++ cypress/e2e/BDSEC/registerpatient.cy.js
@@ -1,41 +1,43 @@
 ///<reference types="cypress" />
 
-import faker from 'faker';
+import faker from 'faker'
+const numberOfDownArrowPresses = Cypress._.random(1, 10);
+
 
 context('Actions', () => {
   beforeEach(() => {
-    cy.baseurl();
-    cy.login();
+    cy.baseurl()
+    cy.login()
   });
 
   it('Register Patient', () => {
-    const fname = faker.name.firstName();
-    const mname = faker.name.firstName();
-    const lname = faker.name.lastName();
+    const fname = faker.name.firstName()
+    const mname = faker.name.firstName()
+    const lname = faker.name.lastName()
 
-    const phone = '07' + Math.floor(Math.random() * 1000000000); // Generate a valid phone number
-    const genders = ['Male', 'Female'];
-    const randomGender = genders[Math.floor(Math.random() * genders.length)];
-    const randomAge = Math.floor(Math.random() * 50) + 1;
+    const phone = '07' + Math.floor(Math.random() * 1000000000) // Generate a valid phone number
+    const genders = ['Male', 'Female']
+    const randomGender = genders[Math.floor(Math.random() * genders.length)]
+    const randomAge = Math.floor(Math.random() * 50) + 1
 
     // Start filling the form
-    cy.waitForLoader();
-    cy.get('#location').select('Registration Desk');
-    cy.get('.confirm').click();
-    cy.waitForLoader();
-    cy.get('.fa-user').click();
-    cy.waitForLoader();
+    cy.waitForLoader()
+    cy.get('#location').select('Registration Desk')
+    cy.get('.confirm').click()
+    cy.waitForLoader()
+    cy.get('.fa-user').click()
+    cy.waitForLoader()
 
-    cy.get('.fa-plus').click({ force: true });
-    cy.waitForLoader();
+    cy.get('.fa-plus').click({ force: true })
+    cy.waitForLoader()
 
     // Type in the fields
-    cy.get('#givenName').type(fname);
-    cy.get('#middleName').type(mname);
-    cy.get('#familyName').type(lname);
+    cy.get('#givenName').type(fname)
+    cy.get('#middleName').type(mname)
+    cy.get('#familyName').type(lname)
     cy.get('#gender').select(randomGender);
-    cy.get('#ageYears').type(randomAge.toString());
-    cy.get('#phoneNumber').type(phone);
+    cy.get('#ageYears').type(randomAge.toString())
+    cy.get('#phoneNumber').type(phone)
 
     // Toggle and select a random visit
     cy.get('button.toggle-button').click();
@@ -46,9 +48,9 @@ context('Actions', () => {
     cy.waitForLoader();
 
     // Submit the form
-    cy.contains('Save').click({ force: true });
+    cy.contains('Save').click({ force: true })
     cy.wait(1000);
-    cy.contains('Back').click({ force: true });
+    cy.contains('Back').click({ force: true })
 
     // Capture patient data for reuse
     cy.get('#patientIdentifierValue')
@@ -63,77 +65,90 @@ context('Actions', () => {
         };
 
         // Store patient data in Cypress environment
-        Cypress.env('patientData', patientData);
+        Cypress.env('patientData', patientData)
 
         cy.log(
           `Registered patient: ${patientData.fname} ${patientData.mname} ${patientData.lname} with ID: ${patientData.registrationNumber}`
-        );
-      });
-  });
+        )
+      })
+  })
 
   it('Clinical Module', () => {
     // Retrieve patient data from Cypress environment
-    const patientData = Cypress.env('patientData');
-    cy.waitForLoader();
-    cy.get('#location').select('Registration Desk');
-    cy.get('.confirm').click();
-    cy.waitForLoader();
-    cy.get('.fa-stethoscope').click();
-    cy.waitForLoader();
+    const patientData = Cypress.env('patientData')
+    cy.waitForLoader()
+    cy.get('#location').select('Registration Desk')
+    cy.get('.confirm').click()
+    cy.waitForLoader()
+    cy.get('.fa-stethoscope').click()
+    cy.waitForLoader()
 
     // Search for the registered patient
-    cy.get('#patientIdentifier', { timeout: 10000 }).should('be.visible');
-    cy.get('#patientIdentifier').type(patientData.fname);
-    cy.waitForLoader();
-
-    cy.get('.smallImages').click();
-    cy.waitForLoader();
-    cy.get('.loader', { timeout: 10000 }).should('not.exist');
-    cy.waitForLoader();
+    cy.get('#patientIdentifier', { timeout: 10000 }).should('be.visible')
+    cy.get('#patientIdentifier').type(patientData.fname)
+    cy.waitForLoader()
+    cy.get('.smallImages').click()
+    cy.waitForLoader()
+    cy.get('.loader', { timeout: 10000 }).should('not.exist')
+    cy.waitForLoader()
 
     // Access the consultation template
-    cy.get('.btn--left').click();
-    cy.wait(5000);
+    cy.get('.btn--left').click()
+    cy.wait(5000)
+    cy.get('#template-control-panel-button').click()
 
-    cy.get('#template-control-panel-button').click();
-
-
-    cy.get('#Amsler_Grid_Test').click();
+    cy.get('#Amsler_Grid_Test').click() //Amsler
     function selectRandomOptionsForAllDropdowns() {
-          cy.get('.obs-control-select-wrapper') // Select all dropdown containers
+          cy.get('.obs-control-select-wrapper') 
             .each(($dropdown) => {
-              // Open each dropdown
-              cy.wrap($dropdown).find('.Select-control').click();
-
-              // Select a random option
+              cy.wrap($dropdown).find('.Select-control').click()
               cy.get('.Select-menu')
-                .find('[role="option"]') // Options inside the opened dropdown
+                .find('[role="option"]')
                 .then((options) => {
-                  const totalOptions = options.length;
+                  const totalOptions = options.length
                   const randomIndex = Math.floor(Math.random() * totalOptions);
-                  cy.wrap(options[randomIndex]).click(); // Select a random option
-                });
-            });
+                  cy.wrap(options[randomIndex]).click()
+                })
+            })
         }
-
-      // Usage in your test
-      selectRandomOptionsForAllDropdowns();
+      selectRandomOptionsForAllDropdowns()
       cy.get(':nth-child(3) > .confirm').click()
+      cy.waitForLoader()
 
+      cy.get('#template-control-panel-button').click()
+      cy.get('#Before_Surgery_Checklist').click({force: true}) //Before Surgery Checklist
+      cy.get('[title="Aseptic precaution taken"]').click()
+      cy.get('[title="Eye cleaned with betadine/Iodine 10%"]').click()
+      cy.get('[title="Draped &speculum applied"]').click()
+      cy.get(':nth-child(3) > .confirm').click()
+      cy.waitForLoader()
 
+      cy.get('#template-control-panel-button').click()
+      cy.get('#Color_Vision_Test').click() //Color vision test
+      cy.get('div#concept-set-2e99a1ea-0de8-40f1-aad4-9f949156d550 .Select-control').should('have.length', 2);
+      cy.get('div#concept-set-2e99a1ea-0de8-40f1-aad4-9f949156d550 .Select-control').first().as('firstDropdown');
+        // Open the first dropdown
+      cy.get('@firstDropdown').click();
+      cy.get('.Select-menu-outer').should('be.visible');
+      cy.get('.Select-option').then(($options) => {
+        const randomIndex = Math.floor(Math.random() * $options.length);
+          cy.get('.Select-option').eq(randomIndex).click();
+      })
+        // Select the second dropdown
+      cy.get('div#concept-set-2e99a1ea-0de8-40f1-aad4-9f949156d550 .Select-control').last().as('secondDropdown');
+        cy.get('@secondDropdown').click();
+        cy.get('.Select-menu-outer').should('be.visible');
+        cy.get('.Select-option').then(($options) => {
+        const randomIndex = Math.floor(Math.random() * $options.length);
+          cy.get('.Select-option').eq(randomIndex).click();
+      });
+      cy.get(':nth-child(3) > .confirm').click()
+    cy.waitForLoader()
 
+   
+    
 
-
-
-
-  });
-
-
-
-
-
-
-
+  })
 /*
 
     it('Make Lab Orders', () => {
