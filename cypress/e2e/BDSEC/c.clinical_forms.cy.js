a1ab95eb5a4dbf044b2fa0da97f26efc27529518
Muyundo <muyundobrian@gmail.com>
2025-02-03 14:10:36 +0300

    Updated clinical module to user intercepts

--- cypress/e2e/BDSEC/c.clinical_forms.cy.js
+++ cypress/e2e/BDSEC/c.clinical_forms.cy.js
@@ -3,24 +3,22 @@ context('Actions', () => {
     beforeEach(() => {
       cy.baseurl()
       cy.login()
+      cy.interceptAPI()
     })
 it('Update Clinical forms', () => {
     cy.readFile('cypress/fixtures/patientData.json').then((patientData) => {
       cy.module()
       cy.get('.fa-stethoscope').click()
-      cy.waitForLoader()
+      cy.wait('@patientsInqueue').its('response.statusCode').should('eq', 200)
       cy.get('#patientIdentifier').should('be.visible').type(patientData.registrationNumber)
-      cy.waitForLoader()
       cy.get('.smallImages').click()
-      cy.waitForLoader()
-      cy.get('.loader').should('not.exist')
-      cy.waitForLoader()
+      cy.wait('@patientDashboard').its('response.statusCode').should('eq', 200)
 
       // Access the consultation template
       cy.get('.btn--left').click()
-      cy.wait(5000)
+      cy.wait('@PublishedForms').its('response.statusCode').should('eq', 200)
       cy.get('#template-control-panel-button').click()
-      cy.waitForLoader()
+      //cy.waitForLoader()
       cy.get('#Amsler_Grid_Test').click()
 
       function selectRandomOptionsForAllDropdowns() {
@@ -46,11 +44,10 @@ it('Verify the forms have been saved', ()=>{
     cy.readFile('cypress/fixtures/patientData.json').then((patientData) => {
     cy.module()
     cy.get('.fa-stethoscope').click()
-    cy.waitForLoader()
+    cy.wait('@patientsInqueue').its('response.statusCode').should('eq', 200)
     cy.get('#patientIdentifier').should('be.visible').type(patientData.registrationNumber)
-    cy.waitForLoader()
     cy.get('.smallImages').click()
-    cy.waitForLoader()
+    cy.wait('@patientDashboard').its('response.statusCode').should('eq', 200)
     cy.contains('.form-name', 'Amsler Grid Test', { timeout: 10000 }) 
       .should('be.visible');
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add("baseurl", () => {
-    cy.visit("http://102.133.147.40/bahmni/home/index.html#/login");
-    //cy.visit('https://bdsec.intellisoftkenya.com/bahmni/home/')
+    //cy.visit("http://102.133.147.40/bahmni/home/index.html#/login");
+    cy.visit('https://bdsec.intellisoftkenya.com/bahmni/home/')
 })
 
 Cypress.Commands.add('login', () => {
@@ -26,12 +26,19 @@ Cypress.Commands.add('waitForLoader', (timeout = 5000) => {
     cy.get('.loader', { timeout: 10000 }).should('not.exist')
 })
 
-Cypress.Commands.add('module', ()=>{
+Cypress.Commands.add('module', ()=>{  //handles logins for each script that needs it
   cy.waitForLoader()
   cy.get('#location').select('Registration Desk')
   cy.get('.confirm').click()
   cy.waitForLoader()
 
+})
+
+Cypress.Commands.add('interceptAPI', ()=> {
+  cy.intercept('GET','**/openmrs/ws/rest/v1/bahmnicore/sql?location_uuid=*&provider_uuid=*&q=emrapi.sqlSearch.activePatientsByProvider&v=full'
+  ).as('patientsInqueue')
+  cy.intercept('POST','**openmrs/ws/rest/v1/user/**').as('patientDashboard')
+  cy.intercept('GET','**openmrs/ws/rest/v1/bahmniie/form/latestPublishedForms' ).as('PublishedForms')
 })
 
 
