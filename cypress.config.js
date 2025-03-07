5accf34d9454e2c697a4e713108b623fad96078d
Muyundo <muyundobrian@gmail.com>
2025-03-07 16:50:38 +0300

    added secret login

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,21 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '6gphnc',
+  projectId: '6gphnc',  // Removed duplicate projectId
   e2e: {
-        pageLoadTimeout: 180000, 
-        experimentalStudio: true,
-        viewportWidth: 1280,   // Set width
-        viewportHeight: 720,   // Set height
-        chromeWebSecurity: false,
+    pageLoadTimeout: 180000, 
+    experimentalStudio: true,
+    viewportWidth: 1280,  
+    viewportHeight: 720,  
+    chromeWebSecurity: false,
 
-        projectId: '6gphnc',
-          
     setupNodeEvents(on, config) {
-      config.env.CYPRESS_USERNAME = process.env.CYPRESS_USERNAME;
-      config.env.CYPRESS_PASSWORD = process.env.CYPRESS_PASSWORD;
-      return config
-        },
-  },
-  exit: false,
+      // Use process.env for GitHub Actions and provide fallbacks
+      config.env.CYPRESS_USERNAME = process.env.CYPRESS_USERNAME || "defaultUser";
+      config.env.CYPRESS_PASSWORD = process.env.CYPRESS_PASSWORD || "defaultPass";
 
+      return config;
+    },
+  },
+  exit: false,  // This is not needed, Cypress exits by default
 });
